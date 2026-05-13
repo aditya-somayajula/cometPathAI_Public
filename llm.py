de0b172dddfd56aa21a5a9a7c987514e9b7854f7
Aditya Somayajula <adityas.sai@gmail.com>
2026-05-12 23:07:57 -0500

    Add multiple LLM options and refactor llm setup

--- llm.py
+++ llm.py
@@ -10,18 +10,38 @@ logging.disable_progress_bar()
 py_logging.getLogger("transformers").setLevel(py_logging.ERROR)
 
 import streamlit as st
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
 from langchain_core.embeddings import Embeddings
 from sentence_transformers import SentenceTransformer
 
-load_dotenv()
 
+# -----------------------------
+# Model Options
+# -----------------------------
+MODEL_OPTIONS = [
+    "Groq - Llama 3.3 70B Versatile",
+    "OpenAI - GPT-4o Mini",
+    "Anthropic - Claude Sonnet 4.5",
+    "Anthropic - Claude Haiku 4.5"
+]
+DEFAULT_MODEL = "Groq - Llama 3.3 70B Versatile"
 
 # -----------------------------
 # LLM Setup
 # -----------------------------
-llm = ChatGroq(model_name="llama-3.3-70b-versatile", temperature=0.5)
+@st.cache_resource
+def get_llm(model_name, model_api):
+    if model_name == "Groq - Llama 3.3 70B Versatile":
+        llm = ChatGroq(model_name="llama-3.3-70b-versatile", temperature=0.5, groq_api_key=model_api)
+    if model_name == "OpenAI - GPT-4o Mini":
+        llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.5, api_key=model_api)
+    if model_name == "Anthropic - Claude Sonnet 4.5":
+        llm = ChatAnthropic(model="claude-sonnet-4-5", temperature=0.5, anthropic_api_key=model_api)
+    if model_name == "Anthropic - Claude Haiku 4.5":
+        llm = ChatAnthropic(model="claude-haiku-4-5", temperature=0.5, anthropic_api_key=model_api)
+    return llm
 
 
 # -----------------------------
