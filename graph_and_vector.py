883321ad7e820b9456434cff5e282f7ea0a3208c
Aditya Somayajula <adityas.sai@gmail.com>
2026-05-14 10:53:00 -0500

    Refactor graph and vector store functions
    Refactor graph and vector store setup to use environment variables instead of session state.

--- graph_and_vector.py
+++ graph_and_vector.py
@@ -1,47 +1,33 @@
 # Importing Libraries
-import streamlit as st
+import os
 from llm import embeddings
 from langchain_neo4j import Neo4jGraph
 from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # -----------------------------
 # Graph Store Setup
 # -----------------------------
-@st.cache_resource
-def get_graph_store(uri, username, password, database):
+def get_graph_main():
     return Neo4jGraph(
-        url=uri,
-        username=username,
-        password=password,
-        database=database
+        url=os.getenv("MAIN_NEO4J_URI"),
+        username=os.getenv("MAIN_NEO4J_USERNAME"),
+        password=os.getenv("MAIN_NEO4J_PASSWORD"),
+        database=os.getenv("MAIN_NEO4J_DATABASE")
     )
 
-def get_graph_main():
-    return get_graph_store(
-        st.session_state["MAIN_NEO4J_URI"],
-        st.session_state["MAIN_NEO4J_USERNAME"],
-        st.session_state["MAIN_NEO4J_PASSWORD"],
-        st.session_state["MAIN_NEO4J_DATABASE"]
-    )
 
 # -----------------------------
 # Vector Store Setup
 # -----------------------------
-@st.cache_resource
-def get_vector_store(collection_name, api_key, tenant, db):
-    return Chroma(
-        collection_name=collection_name,
-        embedding_function=embeddings,
-        chroma_cloud_api_key=api_key,
-        tenant=tenant,
-        database=db
-    )
-
 def get_vector_main():
-    return get_vector_store(
-        st.session_state["COLLECTION_NAME"],
-        st.session_state["CHROMA_API_KEY"],
-        st.session_state["CHROMA_TENANT"],
-        st.session_state["CHROMA_DB"]
+    return Chroma(
+        collection_name=os.getenv("COLLECTION_NAME"),
+        embedding_function=embeddings,
+        chroma_cloud_api_key=os.getenv("CHROMA_API_KEY"),
+        tenant=os.getenv("CHROMA_TENANT"),
+        database=os.getenv("CHROMA_DB")
     )
