7aced2eddc3969051c83d7f56cf3c68624964112
Aditya Somayajula <adityas.sai@gmail.com>
2026-05-14 10:55:07 -0500

    Add hybrid vector search with BM25 and reranker

--- tools.py
+++ tools.py
@@ -1,8 +1,10 @@
 # Importing Libraries
 import json
+import numpy as np
 import streamlit as st
-from llm import get_llm
+from llm import get_llm, reranker
 from langchain.tools import tool
+from rank_bm25 import BM25Okapi
 from prompts import CYPHER_GENERATION_TEMPLATE
 from langchain_neo4j import GraphCypherQAChain
 from langchain_core.prompts import PromptTemplate
@@ -62,7 +64,7 @@ def graph_search(question: str):
         Input should always be a natural language question.
     """
     graph = get_graph_main()
-    llm_model = get_llm(st.session_state.model_name, st.session_state["MODEL_API_KEY"])
+    llm_model = get_llm(st.session_state.model_name, st.session_state.model_api_key)
 
     cypher_prompt = PromptTemplate.from_template(CYPHER_GENERATION_TEMPLATE)
 
@@ -84,3 +86,99 @@ def graph_search(question: str):
     if not result:
         return "No graph results found."
     return result
+
+
+# -----------------------------
+# Hybrid Vector Search (Dense + Sparse) & Reranker
+# -----------------------------
+def hybrid_vector_search(docs, filter_type):
+    try:
+        if type(docs) != list:
+            docs = [docs]
+
+        print(filter_type)
+
+        vector_store = get_vector_main()
+
+        # Fetch Relevant Vector DB Records
+        all_data = vector_store.similarity_search(query="", k=300, filter={"type": filter_type})
+        all_documents = all_data["documents"]
+        all_ids = all_data["ids"]
+        all_metadata = all_data["metadatas"]
+        print("Fetched all dta from vector store")
+
+        # BM25 Tokenizer
+        tokenized_docs = [
+            doc.lower().split()
+            for doc in all_documents
+        ]
+        bm25 = BM25Okapi(tokenized_docs)
+        print("Tokenizer")
+
+        all_candidates = []
+        for doc in docs:
+            # Vector Search
+            vector_results = vector_store.similarity_search_with_score(query=doc, k=50, filter={"type": filter_type})
+
+            for result_doc, score in vector_results:
+                candidate = {
+                    "id": result_doc.metadata.get("id"),
+                    "text": result_doc.page_content,
+                    "metadata": result_doc.metadata,
+                    "distance": score
+                }
+                all_candidates.append(candidate)
+
+            # BM25 retrival
+            tokenized_query = doc.lower().split()
+            bm25_scores = bm25.get_scores(tokenized_query)
+            top_indices = np.argsort(bm25_scores)[::-1][:5]
+            for idx in top_indices:
+                candidate = {
+                    "id": all_ids[idx],
+                    "text": all_documents[idx],
+                    "metadata": all_metadata[idx],
+                    "bm25_score": bm25_scores[idx]
+                }
+                all_candidates.append(candidate)
+        print("Got all candidates")
+
+        unique_candidates = {}
+        for candidate in all_candidates:
+            unique_candidates[candidate["id"]] = candidate
+        candidate_docs = list(unique_candidates.values())
+        print("Got unique candidates")
+
+        combined_query = " ".join(docs)
+        pairs = [
+            (combined_query, doc["text"])
+            for doc in candidate_docs
+        ]
+        print("Combined Query")
+
+        # Re-rank
+        scores = reranker.predict(pairs)
+        reranked_results = sorted(
+            zip(candidate_docs, scores),
+            key=lambda x: x[1],
+            reverse=True
+        )
+        print("Rerannked")
+
+        TOP_K = 10
+        results_str = ""
+        for rank, (doc, score) in enumerate(
+                reranked_results[:TOP_K],
+                start=1
+        ):
+            results_str += f"Rank #{rank} \n"
+            results_str += f"Rerank Score: {score:.4f}\n"
+            results_str += f"Content #{doc.get('text')} \n"
+            results_str += f"Metadata #{doc.get('metadata')} \n"
+            results_str += "\n\n ----- ----- ----- \n\n"
+        print("Finished")
+
+        return results_str
+    except Exception as e:
+        print(e)
+        return "Unable to get results from vector database."
