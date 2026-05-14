7fe4b56187cd569d818602344172d9a6573d25da
Aditya Somayajula <adityas.sai@gmail.com>
2026-05-14 16:30:56 -0500

    Refactor helper.py for improved clarity and structure
    Refactor school inference logic and update constants.

--- helper.py
+++ helper.py
@@ -13,7 +13,7 @@ from graph_and_vector import get_vector_main, get_graph_main
 CHUNK_SIZE = 750
 CHUNK_OVERLAP = 50
 BEST_CHUNK_SIZE = 3
-SCHOOL_COURSE_INFERENCE_K = 50
+COURSE_INFERENCE_K = 50
 TOP_K_SCHOOLS = 1
 VECTOR_RETRIVAL_WEIGHT = 0.3
 GRAPH_RETRIVAL_WEIGHT = 0.7
@@ -53,12 +53,12 @@ def extract_resume_keywords(llm_model) -> list[str]:
 # -----------------------------
 # School Inference
 # -----------------------------
-def infer_school_context(resume_text, keywords):
+def infer_school_context(keywords):
     try:
         vector_db = get_vector_main()
         graph_db = get_graph_main()
 
-        resume_docs = vector_db.similarity_search(st.session_state.resume_data, k=SCHOOL_COURSE_INFERENCE_K, filter={"type": "Course"})
+        resume_docs = vector_db.similarity_search(st.session_state.resume_data, k=COURSE_INFERENCE_K, filter={"type": "Course"})
         resume_schools = [d.metadata.get("school") for d in resume_docs if d.metadata.get("school")]
         fold1_counts = {}
         for school in resume_schools:
@@ -73,13 +73,6 @@ def infer_school_context(resume_text, keywords):
             RETURN s.name AS school, count(c) AS course_count
             ORDER BY course_count DESC
             """
-            # cypher_query = """
-            #     UNWIND $keywords AS skill_name
-            #     MATCH (sk:Skill)<-[:TEACHES]-(c:Course)<-[:HAS_COURSE]-(s:School)
-            #     WHERE sk.name =~ ('(?i).*' + skill_name + '.*')
-            #     RETURN s.name AS school, count(c) AS course_count
-            #     ORDER BY course_count DESC
-            #     """
             try:
                 graph_results = graph_db.query(cypher_query, params={"keywords": keywords})
                 fold2_counts = {r['school']: r['course_count'] for r in graph_results}
